77c8755f4dbc0ab8b995d1131011b44384b26b82
Willian dos Anjos Boaventura <williandosanjos@gmail.com>
2026-02-27 12:58:57 -0300

    adicionado classes security

---
+++ src/main/java/com/backend/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,32 @@
+package com.backend.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "numero")
+    private Long numero;
+    @Column(name = "bairro")
+    private String bairro;
+    @Column(name = "complemento", length = 50)
+    private String complemento;
+    @Column(name = "cidade", length = 150)
+    private String cidade;
+    @Column(name = "estado", length = 2)
+    private String estado;
+    @Column(name = "cep", length = 9)
+    private String cep;
+
+}
